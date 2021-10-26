82c9db7d85e7ddf48cf57cfe0b88c8700ea6128a
Martyna <martyna.szczekocka@gmail.com>
2021-10-26 20:58:29 +0200

    Add class which get status from client

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -1,9 +1,13 @@
 package client;
-
+/* utworzyc tablice
+    definiowac metody
+    do kazdej metody check
+ */
 public class Main {
 
     public static void main(String[] args) {
-        //
-        System.out.println("Hello, world!");
+        StatusGetter statusGetter = new StatusGetter();
+
+        System.out.println(statusGetter.getStatus());
     }
 }

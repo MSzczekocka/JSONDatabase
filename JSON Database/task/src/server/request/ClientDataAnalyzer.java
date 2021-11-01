ea12f86249e7b1cb52ce68fb64975a3840ffe72e
Martyna <martyna.szczekocka@gmail.com>
2021-11-01 10:16:30 +0100

    Add tests for ClientDataAnalyzer

--- JSON Database/task/src/server/request/ClientDataAnalyzer.java
+++ JSON Database/task/src/server/request/ClientDataAnalyzer.java
@@ -5,14 +5,11 @@ import java.util.Arrays;
 public class ClientDataAnalyzer {
     public String[] getCommand(String input) {
         String[] info = input.split(" ");
-        if (checkIndex(info[1]).equals("ERROR")) info[1] = "505";
+        if (!checkIndex(info[1])) info[1] = "505";
         return info;
     }
 
-    private static String checkIndex(String index) {
-        if (Integer.getInteger(index) > 100 || Integer.getInteger(index) < 1) {
-            return "ERROR";
-        }
-        return "OK";
+    private static boolean checkIndex(String index) {
+        return Integer.parseInt(index) <= 100 && Integer.parseInt(index) >= 1;
     }
 }

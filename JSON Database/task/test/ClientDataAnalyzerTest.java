630fcaaf6a104cfb925fdad899b1402442545986
Martyna <martyna.szczekocka@gmail.com>
2021-11-05 13:49:26 +0100

    Fix test - ClientDataAnalyzer

--- JSON Database/task/test/ClientDataAnalyzerTest.java
+++ JSON Database/task/test/ClientDataAnalyzerTest.java
@@ -7,7 +7,7 @@ public class ClientDataAnalyzerTest {
     public void checkIndex_okTest(){
         //given
         ClientDataAnalyzer clientDataAnalyzer = new ClientDataAnalyzer();
-        String[] request = {"set","45","HelloWorld"};
+        String[] request = {"set","45","Hi"};
         //when
         boolean result = clientDataAnalyzer.checkIndex(request);
         //then
@@ -18,7 +18,18 @@ public class ClientDataAnalyzerTest {
     public void checkIndex_nokTest(){
         //given
         ClientDataAnalyzer clientDataAnalyzer = new ClientDataAnalyzer();
-        String[] request = {"set","44647","HelloWorld"};
+        String[] request = {"set","56783","Hi"};
+        //when
+        boolean result = clientDataAnalyzer.checkIndex(request);
+        //then
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void checkIndex_nok2Test(){
+        //given
+        ClientDataAnalyzer clientDataAnalyzer = new ClientDataAnalyzer();
+        String[] request = {"set","-4","Hi"};
         //when
         boolean result = clientDataAnalyzer.checkIndex(request);
         //then
